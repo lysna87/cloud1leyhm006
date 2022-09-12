8f56d93490e3cf25574a790c6573139048ac66ac
Heather L <leyhm006@mymail.unisa.edu.au>
2022-09-12 12:20:26 +0930

    export change

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,23 +2,27 @@
 import React from 'react';
 import Amplify from 'aws-amplify';
 //2.
-import awsExports from './aws-exports';
+//import awsExports from './aws-exports';
+import awsmobile from './aws-exports';
 //3.
 //import {AmplifySignOut, withAuthenticator} from '@aws-amplify/ui-react'
 //import Authenticator
 import { Authenticator, withAuthenticator} from '@aws-amplify/ui-react'
 
 //4.
-Amplify.configure(awsExports)
+Amplify.configure(awsmobile)
+//Amplify.configure(awsExports)
 
 function Dashboard() {
  return(
     <Authenticator>
       {({ signOut, user }) => (
         <div className="App">
+        <p>Placeholder for icon</p>
           <p>
             Hey {user.username}, welcome to the Nuber Driver page
           </p>
+          <p>Placeholder for File upload</p>
           <button onClick={signOut}>Sign out</button>
         </div>
       )}
@@ -26,5 +30,5 @@ function Dashboard() {
  )
 }
 
-//6.
+//Require Authentication
 export default withAuthenticator(Dashboard)
